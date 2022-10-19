ae56207b369750942b44bab425bd29e0fb502e4c
rahulho <rahulkumarthakurofficial123@gmail.com>
2022-10-19 11:54:44 +0545

    for loop in javascript

---
+++ javasp/forloop.js
@@ -0,0 +1,17 @@
+// for (let i = 0; i <= 100; i+= 2){
+    // console.log("this is rahul");
+// }
+/*
+let i = 0;
+while (i<=100){
+    console.log("i am good");
+    i += 2;
+}
+*/
+let j = 1;
+do 
+{
+    console.log("the value of j = " + j);
+    j += 2;
+}
+while (j <= 10);
